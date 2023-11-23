ac178bb44a8a88bf2f23a6240e2872a46540c150
Matteo Migliarini <matteo.migliarini.01@gmail.com>
2023-11-23 23:35:36 +0100

    reshape grasp

--- funcs/GRASP.py
+++ funcs/GRASP.py
@@ -14,11 +14,15 @@ class GRASP:
 
     def search(self, s:int, A: npt.NDArray[np.int_], W: npt.NDArray[np.int_]) -> Solution:
         greedy_search = GreedySPlex(A, W, s=s)
-        for _ in range(self.trials):
-            A1, splexes = greedy_search.random_solution()
-
-            x0 = Solution(A,W,A1,splexes)
-            x1 = self.local_search.search(x0)
-            self.solutions_found.append(x1)
+        
+        solutions = [self.grasp_instance(greedy_search, A, W) for _ in range(self.trials)]
+        self.solutions_found.extend(solutions)
 
         return min(self.solutions_found, key=lambda x: x.obj())
+    
+    def grasp_instance(self, greedy_search : GreedySPlex, A: npt.NDArray[np.int_], W: npt.NDArray[np.int_]):
+        A1, splexes = greedy_search.random_solution()
+
+        x0 = Solution(A,W,A1,splexes)
+        x1 = self.local_search.search(x0)
+        return x1
