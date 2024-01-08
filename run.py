7fa9cd691b18ae81296ff1a3ee1a7d1e2f815056
NicolaMaestri00 <104208237+NicolaMaestri00@users.noreply.github.com>
2024-01-08 09:30:15 +0100

    .

--- run.py
+++ run.py
@@ -2,9 +2,14 @@ from funcs import readin, Solution, GA, is_splex
 from funcs.Greedy import Karger
 import numpy as np
 
-file = 'data/test_instances/heur030_n_330_m_5613.txt'
+# file = 'data/test_instances/heur030_n_330_m_5613.txt'
 # file = 'data/test_instances/heur022_n_322_m_14226.txt'
 # file = 'data/test_instances/heur039_n_361_m_13593.txt'
+
+
+# file = 'data\inst_competition\heur049_n_300_m_17695.txt'
+file = 'data\inst_competition\heur050_n_300_m_19207.txt'
+# file = 'data\inst_competition\heur051_n_300_m_20122.txt'
 
 S,A,W = readin(file)
 greedy = Karger(A, W, S)
@@ -25,7 +30,7 @@ GeneticAlgorithm = GA.GeneticAlgorithm(initial_population, A, S)
 
 print(np.mean(GeneticAlgorithm.get_fitness()))
 final_population = GeneticAlgorithm.evolution(3000)
-print(np.mean([solution.obj() for solution in final_population]))
+print(np.min([solution.obj() for solution in final_population]))
 
 # for i in range(len(final_population)):
 #     if  not is_splex(final_population[i].A1, S):
